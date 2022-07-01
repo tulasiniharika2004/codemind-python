0a1f313955ec7b1eac69f9ccd9708151b8e5d36e
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-07-01 08:52:26 +0530

    def prime(n):     if n<2:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True n=int(input()) m=int(input()) c=0 for j in range(n,m+1):     if prime(j):         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,16 +1,14 @@
 def prime(n):
     if n<2:
         return False
-    else:
-        return True
     for i in range(2,int(n**0.5)+1):
-            if n%i==0:
-                return False
+        if n%i==0:
+            return False
     return True
-a=int(input())
-b=int(input())
+n=int(input())
+m=int(input())
 c=0
-for i in range(a,b+1):
-    if prime(i):
+for j in range(n,m+1):
+    if prime(j):
         c+=1
 print(c)        
