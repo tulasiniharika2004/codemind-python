86bdbac69997f9d7ca7deadbb97d782499d48204
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-04 18:07:50 +0530

    n=int(input()) a=0 b=1 c=0 for i in range(2,n+1):     sum=a+b     b=a     a=sum     if(n==sum):         print("True")         c=1 if(c==0):     print("False")

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,14 +1,13 @@
-def fib(l):
-    a,b=0,1
-    c1=0
-    for i in range(2,l):
-        c=a+b
-        if(l==c):
-            c1=1
-        b,a=c,b
-    if c1==1:
-        print(True)
-    else:
-        print(False)
 n=int(input())
-fib(n)
+a=0
+b=1
+c=0
+for i in range(2,n+1):
+    sum=a+b
+    b=a
+    a=sum
+    if(n==sum):
+        print("True")
+        c=1
+if(c==0):
+    print("False")
