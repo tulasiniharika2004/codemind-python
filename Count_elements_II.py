63b3def32d8140dd82b2bf6b7ab522aaee61cd7b
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-06 07:42:43 +0530

    a,b=map(int,input().split()) x=list(map(int,input().split())) y=list(map(int,input().split())) c=set(x) d=set(y) s=0 k=0 count=0 for i in c:     if i not in d:         s+=1 for j in d:     if j not in c:         k+=1 print(s+k)

---
+++ Count_elements_II.py
@@ -0,0 +1,15 @@
+a,b=map(int,input().split())
+x=list(map(int,input().split()))
+y=list(map(int,input().split()))
+c=set(x)
+d=set(y)
+s=0
+k=0
+count=0
+for i in c:
+    if i not in d:
+        s+=1
+for j in d:
+    if j not in c:
+        k+=1
+print(s+k)
