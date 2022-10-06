ea974efa152bab63b0ad4398a9b09069e8e833ef
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-06 07:57:20 +0530

    e,f=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) x=[] y=[] for i in a:     if i not in x:         x.append(i) for i in b:     if i not in y:         y.append(i) for i in x:     if i not in y:         print(i,end=" ") for i in y:     if i not in x:         print(i,end=" ")

---
+++ Common_elements_II.py
@@ -0,0 +1,18 @@
+
+e,f=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+x=[]
+y=[]
+for i in a:
+    if i not in x:
+        x.append(i)
+for i in b:
+    if i not in y:
+        y.append(i)
+for i in x:
+    if i not in y:
+        print(i,end=" ")
+for i in y:
+    if i not in x:
+        print(i,end=" ")    
