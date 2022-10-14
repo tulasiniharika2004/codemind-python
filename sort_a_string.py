675b0bb5b548f3cfe399e491132431254f25407b
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-14 21:28:31 +0530

    a=input().split() b=[] c=[] for i in a:     for j in i:         if j.isalpha():             b.append(j)         else:             c.append(j) b.sort() k=0 l=0 for i in a:     for j in range(len(i)):         if i[j] in b:             print(b[k],end='')             k+=1         else:             print(c[l],end='')             l+=1     print(end=' ')

--- sort_a_string.py
+++ sort_a_string.py
@@ -1,15 +1,21 @@
-a=input()
-x=""
-k=""
-i=0
-for j in a:
-    if j in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        x+=j
-x=sorted(x)
-for j in a:
-    if j not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        k+=j
-    else:
-        k+=x[i]
-        i+=1
-print(k)
+a=input().split()
+b=[]
+c=[]
+for i in a:
+    for j in i:
+        if j.isalpha():
+            b.append(j)
+        else:
+            c.append(j)
+b.sort()
+k=0
+l=0
+for i in a:
+    for j in range(len(i)):
+        if i[j] in b:
+            print(b[k],end='')
+            k+=1
+        else:
+            print(c[l],end='')
+            l+=1
+    print(end=' ')
