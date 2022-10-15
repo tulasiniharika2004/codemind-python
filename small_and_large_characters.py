5be6d0fb0390992b3519c0c2f5eac0fce2ccfcb7
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-15 21:37:17 +0530

    n=list(map(str,input().split())) a=[] for i in n:     a.append(min(i))     a.append(max(i)) for i in a:     print(i,end=' ')

--- small_and_large_characters.py
+++ small_and_large_characters.py
@@ -1,5 +1,7 @@
-a=input()
-a=a.split()
+n=list(map(str,input().split()))
+a=[]
+for i in n:
+    a.append(min(i))
+    a.append(max(i))
 for i in a:
-    print(min(i),end=" ")
-    print(max(i),end=" ")
+    print(i,end=' ')
