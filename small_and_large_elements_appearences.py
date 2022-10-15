3273b904f94108746ef93bafb03cbf288304dca8
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-15 19:48:34 +0530

    s=input() s="".join(list(s.split(" "))) print(min(s),s.count(min(s)),end=" ") print(max(s),s.count(max(s)),end=" ")

--- small_and_large_elements_appearences.py
+++ small_and_large_elements_appearences.py
@@ -1,10 +1,4 @@
-a=input()
-a=list(a)
-for i in a:
-    if i==" ":
-        a.remove(i)
-b=set(a)
-print(min(b),end=" ")
-print(a.count(min(b)),end=" ")
-print(max(b),end=" ")
-print(a.count(max(b)),end=" ")
+s=input()
+s="".join(list(s.split(" ")))
+print(min(s),s.count(min(s)),end=" ")
+print(max(s),s.count(max(s)),end=" ")
