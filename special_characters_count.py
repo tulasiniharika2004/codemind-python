d36ab290dcc0157cd9c77a3a74f22a093ec2f411
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-16 07:04:29 +0530

    s=input().lower() c=0 for i in s:     if i not in ' abcdefghijklmnopqrstuvwxyz':         c+=1 print(c)

--- special_characters_count.py
+++ special_characters_count.py
@@ -1,6 +1,6 @@
-s=input()
+s=input().lower()
 c=0
 for i in s:
-    if i not in ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+    if i not in ' abcdefghijklmnopqrstuvwxyz':
         c+=1
 print(c)        
