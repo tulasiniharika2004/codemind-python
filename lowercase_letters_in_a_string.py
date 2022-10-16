046ee13475610f8b23c9b05efd04ca3775d31aaf
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-16 07:09:40 +0530

    s=input() c=0 for i in s:     if ord(i)>=97 and ord(i)<=122:         c+=1 print(c)

--- lowercase_letters_in_a_string.py
+++ lowercase_letters_in_a_string.py
@@ -1,6 +1,6 @@
-a=input()
-count=0
-for i in a:
-    if(ord(i)>=97 and ord(i)<=122):
-        count+=1
-print(count)
+s=input()
+c=0
+for i in s:
+    if ord(i)>=97 and ord(i)<=122:
+        c+=1
+print(c)        
