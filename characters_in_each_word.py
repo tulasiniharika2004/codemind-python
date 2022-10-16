da9497305eea35589754e6d1e2e9371e7523fdb1
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-16 09:44:15 +0530

    s=str(input()) c=0 a=0 for i in range (0,len(s)):     c+=1     if(s[i]==' '):         print(c-1,end=' ')         c=0     elif(i==len(s)-1):         print(c,end=' ')

--- characters_in_each_word.py
+++ characters_in_each_word.py
@@ -1,5 +1,10 @@
-n=input()
-n=n.split()
-for i in n:
-    i=str(i)
-    print(len(i),end=' ')
+s=str(input())
+c=0
+a=0
+for i in range (0,len(s)):
+    c+=1
+    if(s[i]==' '):
+        print(c-1,end=' ')
+        c=0
+    elif(i==len(s)-1):
+        print(c,end=' ')
