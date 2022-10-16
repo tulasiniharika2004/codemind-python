777e019cbdb2560a8221ebfd4d2319872d88f1c0
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-16 07:03:05 +0530

    s=input() c=0 for i in s:     if i not in ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':         c+=1 print(c)

--- special_characters_count.py
+++ special_characters_count.py
@@ -1,15 +1,6 @@
-a=input()
-count=0
-count1=0
-count2=0
-count3=0
-for i in a:
-    if(ord(i)>=33 and ord(i)<=47):
-        count+=1
-    if(ord(i)>=58 and ord(i)<=64):
-        count1+=1
-    if(ord(i)>=91 and ord(i)<=96):
-        count2+=1
-    if(ord(i)>=123 and ord(i)<=126):
-        count+=1
-print(count+count1+count2+count3)
+s=input()
+c=0
+for i in s:
+    if i not in ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+        c+=1
+print(c)        
